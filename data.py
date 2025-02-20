3bd71bc36a9cb376413f503111407e44cfcf038a
DrownUnity <pabloesaaray@gmail.com>
2025-02-20 14:46:54 -0500

    Group by genre

--- data.py
+++ data.py
@@ -6,17 +6,28 @@ netflix_df = pd.read_csv("./netflix_data.csv")
 # Changing data type
 
 netflix_df["title"] = netflix_df['genre'].astype('string')
-netflix_df["type"] = netflix_df['type'].astype('category')
-netflix_df["genre"] = netflix_df['genre'].astype('category')
+netflix_df["type"] = netflix_df['type'].astype('string')
+netflix_df["genre"] = netflix_df['genre'].astype('string')
 netflix_df["country"] = netflix_df["country"].astype('string')
 
-# Data exploration
+print(netflix_df.info())
 
-unique_genres = set(netflix_df['genre'])
+# Deleting Shows from the DB
 
-genres = []
+netflix_df.drop(netflix_df[netflix_df['type'] == 'TV Show'].index, inplace=True)
 
-for genre in unique_genres:
-    genres.append(genre)
+# Group by genre
 
-print(genres)
+genre_counts = netflix_df['genre'].value_counts()
+
+plt.figure(figsize=(10, 6))
+genre_counts.plot(kind='bar', color='skyblue')
+plt.title('Movies per Genre')
+plt.xlabel('Genre')
+plt.ylabel('Amount of Movies')
+plt.xticks(range(len(genre_counts.index)), genre_counts.index, rotation=45)
+plt.show()
+
+# Average duration
+
+print(netflix_df["duration"].mean())
